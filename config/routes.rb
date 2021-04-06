b1dfa7ab187e5c68c2f46984f7a5bf96156b0470
ohletamyleta <ohletamyleta@gmail.com>
2021-04-06 10:45:20 -0400

    add create route to weapons

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :api do
     namespace :v1 do
-      resources :weapons, only: [:index]
+      resources :weapons, only: [:index, :create]
     end
   end
 end
