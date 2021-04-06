f0016b0f1ff2f22e6dbeba02fea5b0348c1c7cbd
ohletamyleta <ohletamyleta@gmail.com>
2021-04-06 09:17:10 -0400

    update migrations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :weapons
+  has_many :weapons, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Weapon.destroy_all
+
+
 blunt = Category.create(name: "Blunt")
 sharp = Category.create(name: "Sharp")
 projectile = Category.create(name: "Projectile")
