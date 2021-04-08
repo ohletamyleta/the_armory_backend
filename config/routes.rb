2e4902c7778ff8044daa0789de1b0c871ad07744
ohletamyleta <ohletamyleta@gmail.com>
2021-04-08 13:57:46 -0400

    add update route for weapons

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :api do
     namespace :v1 do
-      resources :weapons, only: [:index, :create]
+      resources :weapons, only: [:index, :create, :update]
       resources :categories, only: [:index]
     end
   end
