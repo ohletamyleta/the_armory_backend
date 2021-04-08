17fbd7fc0ed7b0099286d7231fc0c56b91748f10
ohletamyleta <ohletamyleta@gmail.com>
2021-04-08 17:58:54 -0400

    add update methods to controller

--- app/controllers/api/v1/weapons_controller.rb
+++ app/controllers/api/v1/weapons_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::WeaponsController < ApplicationController
 
+  before_action :find_weapon, only: [:update]
+
   def index
     weapons = Weapon.all 
     render json: WeaponSerializer.new(weapons)
@@ -14,13 +16,25 @@ class Api::V1::WeaponsController < ApplicationController
     end 
   end
 
+  def update
+    @weapon.update(weapon_params)
+    if @weapon.save
+      render json: @weapon, status: :accepted
+    else
+      render json: { errors: weapon.errors.full_messages }, status: :unprocessible_entity
+    end 
+  end 
+
+
   private
 
     def weapon_params
       params.permit(:name, :description, :video_url, :category_id, :origin, :image_url)
     end
 
-
+    def find_weapon
+      @weapon = Weapon.find(params[:id]W
+    end
 
 
 end
